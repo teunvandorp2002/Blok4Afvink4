bb0c8185a5fc2f5668d886e777759e89b59dd438
teunvandorp2002 <teunvandorp2002@users.noreply.github.com>
2020-05-31 11:34:34 +0200

    created all

--- sequence_processor.py
+++ sequence_processor.py
@@ -5,6 +5,12 @@ import re
 
 
 def sequence_type(seq):
+    """Uses a regex to determine what type of sequence is given
+
+    :param seq: A string with the input sequence
+    :return: The type of sequence and the sequence with just de letters
+             (without spaces and enters)
+    """
     seq = ''.join(filter(str.isalpha, seq)).upper()
     if re.search("^[ACTG]*$", seq):
         return "DNA", seq
@@ -17,6 +23,11 @@ def sequence_type(seq):
 
 
 def dna_processor(seq):
+    """Transcribes and translates a DNA sequence
+
+    :param seq: A string with the DNA sequence
+    :return: A string with the RNA and protein sequences
+    """
     dna = str()
     sequence = Seq(seq, IUPAC.unambiguous_dna)
     dna += 'DNA sequence: ' + sequence + '\n'
@@ -26,15 +37,25 @@ def dna_processor(seq):
 
 
 def blast(seq):
+    """Blasts a protein sequence with blastp and writes the results to
+    an XML file
+
+    :param seq: A string with the protein sequence
+    :return: An XML file with the blast results
+    """
     results = NCBIWWW.qblast(program='blastp', database='nr', sequence=seq)
-    with open('BLAST_results.bxml', "w") as out:
+    with open('BLAST_results.xml', "w") as out:
         out.write(results.read())
     results.close()
 
 
 def parse():
+    """Reads the blast XML file and returns the results
+
+    :return: A srting with the results of the blast search
+    """
     return_string = str()
-    with open("BLAST_results.bxml", "r") as blast_file:
+    with open("BLAST_results.xml", "r") as blast_file:
         result = NCBIXML.read(blast_file)
         for alignment in result.alignments:
             for hsp in alignment.hsps:
